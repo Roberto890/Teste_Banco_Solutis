18d456aee352b9577bf546baf5c4ca933cc9c1db
Roberto <roberto.jesus.amaral@gmail.com>
2021-08-16 11:56:47 -0300

    Criacao da splash activity

---
+++ app/src/main/java/com/rj/testebancosolutis/view/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.rj.testebancosolutis.view
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import com.rj.testebancosolutis.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(LayoutInflater.from(this))
+        val view = binding.root
+        setContentView(view)
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000)
+
+    }
+}
